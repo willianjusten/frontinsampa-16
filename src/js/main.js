5a00b10de8d5d71f72f5aa2f4288eaceb7070ff7
Willian Justen <willianjustenqui@gmail.com>
2016-06-09 14:37:53 -0300

    Remove progress and controls

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,25 @@
 Reveal.initialize({
-	controls: true,
-	progress: true,
-	history: true,
-	center: true,
+    controls: false,
+    progress: false,
+    history: true,
+    center: true,
 
-	transition: 'fade', // none/fade/slide/convex/concave/zoom
+    transition: 'fade', // none/fade/slide/convex/concave/zoom
 
-	// Optional reveal.js plugins
-	dependencies: [
-		{ src: '/js/vendor/classList.js', condition: function() { return !document.body.classList; } },
-		{ src: 'js/vendor/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
-		{ src: 'js/vendor/notes/notes.js', async: true }
-	]
+    // Optional reveal.js plugins
+    dependencies: [
+        { src: '/js/vendor/classList.js', condition: function() { return !document.body.classList; } },
+        { src: 'js/vendor/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+        { src: 'js/vendor/notes/notes.js', async: true }
+    ]
 });
 
-
+var played = false;
 function heartBeat() {
-    document.getElementById('heart').classList.add('activated');
-    var audio = new Audio('musics/heartbeat.mp3');
-	audio.play();
+    if(!played) {
+        document.getElementById('heart').classList.add('activated');
+        var audio = new Audio('musics/heartbeat.mp3');
+        audio.play();
+    }
+    played = true;
 }
